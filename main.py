24772a1c422c070075d0a1676af95a4b877cf0d6
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2024-11-30 18:05:59 +0300

    Подготовил проект к публикации

--- main.py
+++ main.py
@@ -5,12 +5,30 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlsplit
 import argparse
-
+import json
 
 def check_for_redirect(response):
     if response.history:
         raise requests.exceptions.HTTPError
 
+def get_category_url(start_page, end_page):
+    all_books_urls = []
+    all_number_books = []
+    for number in range(start_page,end_page):
+        tululu_url =  f"https://tululu.org/l55/{number}"
+        response = requests.get(tululu_url)
+        response.raise_for_status()
+        check_for_redirect(response)
+        soup = BeautifulSoup(response.text, "html.parser")
+        book_selector = "table.d_book"
+        book_urls = soup.select(book_selector)
+        for book_url in book_urls:
+            url = book_url.find("a")["href"]
+            full_url = urljoin(tululu_url,url)
+            split_url = urlsplit(url).path.split("/")[1]
+            all_books_urls.append(full_url)
+            all_number_books.append(split_url)
+    return all_books_urls, all_number_books
 
 def download_txt(url, filename, book_id, folder="books/"):
     params = {"id":book_id} 
@@ -34,15 +52,18 @@ def download_image(url, folder="images/"):
         file.write(response.content)
 
 
-def parse_book_page(response, book_url):
+def parse_book_page(response, book_url): 
     soup = BeautifulSoup(response.text, "lxml")
-    title_book = soup.find("h1").text
+    title_book = soup.select_one("h1").text
     title, author = title_book.split(" :: ")
-    book_image_url = soup.find("div", class_="bookimage").find("img")["src"]
+    book_image_selector = "div.bookimage img"
+    book_image_url = soup.select_one(book_image_selector)["src"]
     full_image_url = urljoin(book_url, book_image_url)
-    book_comments = soup.find_all("div", class_="texts")
-    comments = [book_comment.find("span", class_="black").text for book_comment in book_comments]
-    book_genres = soup.find("span", class_="d_book").find_all("a")
+    book_comments_selector = "div.texts span.black"
+    book_comments = soup.select(book_comments_selector)
+    comments = [book_comment.text for book_comment in book_comments]
+    book_genres_selector = "span.d_book a"
+    book_genres = soup.select(book_genres_selector)
     genres = [genre.text for genre in book_genres]
     about_book = {
         "genres": genres,
@@ -53,30 +74,38 @@ def parse_book_page(response, book_url):
     }
     return about_book
 
-
 def main():
     parser = argparse.ArgumentParser(
         description="Программа скачивает готовые книги с сайта с оболжками."
     )
-    parser.add_argument("--start_id", type=int, default=1, help="Номер начальной книги")
-    parser.add_argument("--end_id", type=int, default=10, help="Номер последней книги")
+    parser.add_argument("--start_page", type=int, default=1, help="Номер начальной книги")
+    parser.add_argument("--end_page", type=int, default=702, help="Номер последней книги")
+    parser.add_argument("--dest_folder", type=str, default="media", help="Путь к каталогу с результатами парсинга: картинкам, книгам, JSON.")
+    parser.add_argument("--skip_imgs", action="store_true", help="Пропускает скачку изображение")
+    parser.add_argument("--skip_txt", action="store_true",  help="Пропускает скачку текста")
     args = parser.parse_args()
-    for book_id in range(args.start_id, args.end_id):
+    all_book_parameters = []
+    book_urls, book_numbers = get_category_url(args.start_page, args.end_page)
+    for book_url, book_number in zip(book_urls, book_numbers):
         try:
             url = f"https://tululu.org/txt.php"
-            book_url = f"https://tululu.org/b{book_id}/"
             response = requests.get(book_url)
             response.raise_for_status()
             check_for_redirect(response)
             about_book = parse_book_page(response, book_url)
+            all_book_parameters.append(about_book)
             filename = about_book["title"].strip()
-            download_image(about_book["full_image_url"])
-            download_txt(url, filename, book_id)
+            if not args.skip_imgs:
+                download_image(about_book["full_image_url"])
+            if not args.skip_txt:
+                download_txt(url, filename, book_number[1:])
         except requests.exceptions.HTTPError:
             print("Книга не найдена")
         except requests.exceptions.ConnectionError:
             print("Повторное подключение к серверу")
             sleep(20)
+    with open ("information_about_book.json","w", encoding='utf8') as json_file:
+        json.dump(all_book_parameters,json_file,ensure_ascii=False)
 
 
 if __name__ == "__main__":
